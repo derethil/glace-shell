5ef92aaa65b8dc75250b555d0dfa62d228eb8706
Jaren Glenn <jarenglenn@gmail.com>
2025-05-10 01:47:00 -0400

    fix(nix): fix node path

--- nix/packages/glace.nix
+++ nix/packages/glace.nix
@@ -5,21 +5,18 @@
   config,
 }: let
   npmDeps = pkgs.importNpmLock.buildNodeModules {
-    npmRoot = ./.;
+    npmRoot = ../../.;
     inherit (pkgs) nodejs;
   };
 
-  glaceSource =
-    pkgs.runCommand "glace-source" {
-      nativeBuildInputs = [pkgs.symlinkJoin];
-    } ''
-      mkdir -p $out
-      cp -r ${builtins.filterSource (path: type: baseNameOf path != "node_modules") ./.}/* $out/
-      mkdir -p $out/node_modules
-      cp -r ${npmDeps}/node_modules/* $out/node_modules/
-      mkdir -p $out/node_modules/astal
-      cp -r ${inputs.astal-src}/lang/gjs/src/* $out/node_modules/astal/
-    '';
+  glaceSource = pkgs.runCommand "glace-source" {} ''
+    mkdir -p $out
+    cp -r ${builtins.filterSource (path: type: baseNameOf path != "node_modules") ../../.}/* $out/
+    mkdir -p $out/node_modules
+    cp -r ${npmDeps}/node_modules/* $out/node_modules/
+    mkdir -p $out/node_modules/astal
+    cp -r ${inputs.astal}/lang/gjs/src/* $out/node_modules/astal/
+  '';
 in {
   default = inputs.ags.lib.bundle {
     inherit pkgs;
