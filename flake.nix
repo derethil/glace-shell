6ef44200a63ab01643a514551a34accacce60390
Jaren Glenn <jarenglenn@gmail.com>
2025-05-10 01:11:05 -0400

    chore(nix): support for aarch64-linux

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux"];
       imports = [inputs.flake-root.flakeModule];
       perSystem = {
         config,
