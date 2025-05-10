2d0331bc6eb04339e6be7c8ba86fb855e33755ad
Jaren Glenn <jarenglenn@gmail.com>
2025-05-10 01:09:03 -0400

    feat: bundle npm packages using nix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
       url = "github:aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {flake-parts, ...}:
@@ -24,16 +28,34 @@
         glace-dev = pkgs.writeShellScriptBin "glace-dev" ''
           ${pkgs.watchexec}/bin/watchexec -r -w $FLAKE_ROOT -- ags run -d $FLAKE_ROOT
         '';
+
+        npmDeps = pkgs.importNpmLock.buildNodeModules {
+          npmRoot = ./.;
+          inherit (pkgs) nodejs;
+        };
+
+        glaceSource = pkgs.runCommand "glace-source" {} ''
+          mkdir -p $out
+          cp -r ${builtins.filterSource (path: type: baseNameOf path != "node_modules") ./.}/* $out/
+
+          mkdir -p $out/node_modules
+          cp -r ${npmDeps}/node_modules/* $out/node_modules/
+
+          mkdir -p $out/node_modules/astal
+          cp -r ${inputs.astal}/lang/gjs/src/* $out/node_modules/astal/
+        '';
       in {
-        packages = {
-          default = inputs.ags.lib.bundle {
-            inherit pkgs;
-            src = ./.;
-            name = "glace";
-            entry = "app.ts";
-            gtk4 = false;
-            extraPackages = [inputs.ags.packages.${system}.agsFull];
-          };
+        packages.default = inputs.ags.lib.bundle {
+          inherit pkgs;
+          src = glaceSource;
+          name = "glace";
+          entry = "app.ts";
+          gtk4 = false;
+          extraPackages = [
+            inputs.ags.packages.${system}.agsFull
+            # For building node modules
+            pkgs.nodejs
+          ];
         };
 
         devShells.default = pkgs.mkShell {
